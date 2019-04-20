2f249c60d0e96c19bbe10eba07b16374f3abd42e
HARIRAK SRITHAM <harirak@hotmail.co.th>
2019-04-20 20:30:10 +0700

    Create Grade Class  and use Private List collection for member. Create AddGrade Method.

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -6,6 +6,13 @@ namespace Grades
 {
     class GradeBook
     {
+        private List<float> _grades;
 
+        public void AddGrade(float grade)
+        {
+            _grades.Add(grade);
+        }
+
+        
     }
 }

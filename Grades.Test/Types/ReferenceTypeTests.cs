eecc3daffda9c34348bf297272725e6f29225297
HARIRAK SRITHAM <harirak@hotmail.co.th>
2019-04-25 12:55:07 +0700

    Create Method for learn type and input output of the method param

--- Grades.Test/Types/ReferenceTypeTests.cs
+++ Grades.Test/Types/ReferenceTypeTests.cs
@@ -6,10 +6,52 @@ using System.Text;
 namespace Grades.Test.Types
 {
     [TestClass]
-    public class ReferenceTypeTests
+    public class TypeTests
     {
         [TestMethod]
-        public void IntergerVariableHoldValue()
+        public void ValueTestPassByValue()
+        {
+            int x = 46;
+            int y = x;
+            Assert.AreEqual(46, y);
+            IncrementNumber(ref x);
+            
+            Assert.AreEqual(47, x);
+        }
+
+        private int IncrementNumber(ref int number) => number += 1;
+
+        [TestMethod]
+        public void ReferenceTypePassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+         
+            GiveBookAName(ref book2);
+           
+            Assert.AreEqual("A Gradebook", book2.Name);
+
+        }
+
+        private void GiveBookAName(ref GradeBook book)
+        {
+            book = new GradeBook();// Create new allocate
+            book.Name = "A Gradebook";
+        }
+
+
+            
+        [TestMethod]
+        public void StringComparisions()
+        {
+            string name1 = "Harirak";
+            string name2 = "harirak";
+            
+            bool result = string.Equals(name1, name2,StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void IntegerVariableHoldValue()
         {
             int x1 = 100;
             int x2 = x1;
